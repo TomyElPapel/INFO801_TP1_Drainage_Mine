14e76d17c2d0f3103b3aa7542adb27d42089d97f
TomyElPapel <thomasburfinp@gmail.com>
2025-02-12 10:05:12 +0100

    gitingore

--- drainagemine/src/main/java/io/github/pspaces/Main.java
+++ drainagemine/src/main/java/io/github/pspaces/Main.java
@@ -3,28 +3,31 @@ package io.github.pspaces;
 import org.jspace.SequentialSpace;
 import org.jspace.Space;
 
-import static java.lang.Thread.sleep;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException throws InterruptedException {
-
-
-
-        Thread t = new Thread(new Environnement(Config.NIVEAU_EAU_DEPART));
-
-        t.start();
-        InterfaceConsole.start();
-
-        t.join();
+    public static void main(String[] args) throws InterruptedException {
 
         Space space = new SequentialSpace();
 
-        Thread t1 = new Thread( new Pompe(space) );
-        t1.start();
+        Thread env = new Thread(new Environnement(Config.NIVEAU_EAU_DEPART));
+        Thread pompe = new Thread( new Pompe(space) );
+
+
+        env.start();
+        env.start();
+
+
 
         // Signal stop pour arrêter tous les threads
-        space.put("STOP");
+        // space.put("STOP");
 
-        t1.join();
+
+        
+        InterfaceConsole.start();
+
+
+        env.join();
+        pompe.join();
+
     }
 }
