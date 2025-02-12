d1435943fdb4dc331804f06e3e51ae0926ef5e34
TomyElPapel <thomasburfinp@gmail.com>
2025-02-12 11:02:52 +0100

    ratio

--- drainagemine/src/main/java/io/github/pspaces/Environnement.java
+++ drainagemine/src/main/java/io/github/pspaces/Environnement.java
@@ -4,8 +4,11 @@ package io.github.pspaces;
 public class Environnement implements Runnable {
 
     private float _niveauEau = 10;
+    private float _niveauMonoxydeCarbone = 10;
+    private float _niveauMethane = 10;
 
-    private boolean _pompeActive = false; 
+    private boolean _pompeActive = false;
+    private boolean _ventilateurActive = false;
 
     static private Environnement _singleton;
 
@@ -18,6 +21,15 @@ public class Environnement implements Runnable {
         return _singleton._niveauEau;
     }
 
+    static public float getNiveauMonoxydeCarbone() {
+        return _singleton._niveauMonoxydeCarbone;
+    }
+
+
+    static public float getNiveauMethane() {
+        return _singleton._niveauMethane;
+    }
+
     @Override
     public void run() {
         _singleton = this;
@@ -28,10 +40,18 @@ public class Environnement implements Runnable {
             try {
                 Thread.sleep(1000);
                 _niveauEau ++;
+                _niveauMethane ++;
+                _niveauMonoxydeCarbone += 2;
                 if (_pompeActive) {
                     _niveauEau -= 4;
                 }
-                System.out.println("Niveau Eau " + _niveauEau);
+
+                if (_ventilateurActive) {
+                    _niveauMethane -= 3;
+                    _niveauMonoxydeCarbone -= 2;
+                }
+
+                System.out.println("Niveau Eau " + _niveauEau + " | " + "Monoxyde de Carbone " + _niveauMonoxydeCarbone + " | " + "Methane " + _niveauMethane);
 
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -43,5 +63,9 @@ public class Environnement implements Runnable {
 
     public static void SetPompeActive(boolean v) {
         _singleton._pompeActive = v;
+    }
+
+    public static void SetVentilateurActive(boolean v) {
+        _singleton._ventilateurActive = v;
     }
 }
