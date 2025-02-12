c8a79376f4d3023250ed2092ea45c281162a18c8
Djapamal <al.leonarduzzi@gmail.com>
2025-02-12 09:57:00 +0100

    implémentation pompe

--- drainagemine/src/main/java/io/github/pspaces/Main.java
+++ drainagemine/src/main/java/io/github/pspaces/Main.java
@@ -1,9 +1,21 @@
 package io.github.pspaces;
 
+import org.jspace.SequentialSpace;
 import org.jspace.Space;
 
+import static java.lang.Thread.sleep;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world!");
+        Space space = new SequentialSpace();
+
+        Thread t1 = new Thread( new Pompe(space) );
+        t1.start();
+
+        // Signal stop pour arrêter tous les threads
+        space.put("STOP");
+
+        t1.join();
     }
 }
--- drainagemine/src/main/java/io/github/pspaces/Pompe.java
+++ drainagemine/src/main/java/io/github/pspaces/Pompe.java
@@ -1,5 +1,6 @@
 package io.github.pspaces;
 
+import org.jspace.ActualField;
 import org.jspace.Space;
 
 public class Pompe implements Runnable {
@@ -9,8 +10,22 @@ public class Pompe implements Runnable {
         this.space = space;
     }
 
+    void pompage() {
+        System.out.println("Pompe: Je pompe");
+    }
+
     @Override
     public void run() {
+        System.out.println("Pompe: Je suis en marche");
+        try{
+            while ((space.queryp(new ActualField("STOP")) == null)) {               // Attends un signal d'arrêt
+                while (space.query(new ActualField("activation_pompe")) != null) {  // Tant que le signal d'activation est présent
+                    pompage();                                                            // Pompe
+                }
 
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
