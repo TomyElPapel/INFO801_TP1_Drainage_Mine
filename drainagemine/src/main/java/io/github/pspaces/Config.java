aec88269ade44bea4e3134e7dab003b5ea88a859
TomyElPapel <thomasburfinp@gmail.com>
2025-02-14 13:25:24 +0100

    config

--- drainagemine/src/main/java/io/github/pspaces/Config.java
+++ drainagemine/src/main/java/io/github/pspaces/Config.java
@@ -4,4 +4,10 @@ public class Config {
     public static float NIVEAU_EAU_DEPART = 10;
     public static float SEUIL_EAU_HAUT = 20;
     public static float SEUIL_EAU_BAS = 0;
+
+    public static float SEUIL_METHANE_BAS=0;
+    public static float SEUIL_METHANE_HAUT=20;
+
+    public static float SEUIL_MONOXYDE_CARBONE_BAS=0;
+    public static float SEUIL_MONOXYDE_CARBONE_HAUT=20;
 }
