ea3c3ca67d54ad7cae00f4d49f51580ac542cdf3
Djapamal <al.leonarduzzi@gmail.com>
2025-04-07 14:06:23 +0200

    Starfoulax

---
+++ livraison/src/main/java/io/github/pspaces/Demande.java
@@ -0,0 +1,3 @@
+package io.github.pspaces;
+
+public class Demande {}
--- livraison/src/main/java/io/github/pspaces/Main.java
+++ livraison/src/main/java/io/github/pspaces/Main.java
@@ -6,6 +6,21 @@ import org.jspace.Space;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        Space space = new SequentialSpace();
+
+        // Create and start the server thread
+        Server server = new Server(space);
+        Thread serverThread = new Thread(server);
+        serverThread.start();
+
+        // Create and start the client thread
+        Client client = new Client(space);
+        Thread clientThread = new Thread(client);
+        clientThread.start();
+
+        // Wait for both threads to finish
+        serverThread.join();
+        clientThread.join();
 
     }
 }
